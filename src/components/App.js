0fd2dcd3896a7b98719a2d8073288ac4654ed580
Kylie Clark <kyliedarlene@icloud.com>
2024-01-17 19:59:04 -0700

    added basic Filter, Grid and Tile components, pig cards are rendering on page load and slightly styled

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "./Nav";
+import Grid from "./Grid";
+import Filter from "./Filter";
 
 import hogs from "../porkers_data";
 
 function App() {
+	const [displayHogs, setDisplayHogs] = useState(hogs)
+
 	return (
 		<div className="App">
 			<Nav />
+			<Grid hogs={hogs} />
 		</div>
 	);
 }
